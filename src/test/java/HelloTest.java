cc3bab09207fee88f8709005446aa97d2d9b3479
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2021-09-29 12:33:35 +0300

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,7 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello Test");
+        System.out.println("about to share on project on Github");
 
     }
 }
